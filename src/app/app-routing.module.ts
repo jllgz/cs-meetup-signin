0c582cbd0075ca8b5c1190cca453eab01fc533d2
Joseph Obligar <you@example.com>
2020-03-07 02:00:21 -0800

    set up routes(removable)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from '@views/sign-in/sign-in.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
